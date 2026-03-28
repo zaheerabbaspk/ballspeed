dc1502d869342dc67cd89032338c4756733ada60
Zaheer Abbas <zaheerstudioz@gmail.com>
2026-03-28 17:36:55 +0500

    .

--- server/node_modules/mediasoup/node/lib/supportedRtpCapabilities.d.ts
+++
@@ -1,4 +0,0 @@
-import type { RouterRtpCapabilities } from './rtpParametersTypes';
-declare const supportedRtpCapabilities: RouterRtpCapabilities;
-export { supportedRtpCapabilities };
-//# sourceMappingURL=supportedRtpCapabilities.d.ts.map
